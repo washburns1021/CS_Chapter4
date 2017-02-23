adce59dc5076eb5e21ea893b039a034fdf7e96b9
washburns1021 <washburns1021@gmail.com>
2017-02-23 14:33:19 -0500

    :last_quarter_moon:
    :boom:

--- CS_Chapter4/MultiplicationTable/Program.cs
+++ CS_Chapter4/MultiplicationTable/Program.cs
@@ -10,6 +10,15 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
+            for (int x = 1; x <= 10; x++)
+            {
+                for (int y = 1; y <= 10; y++)
+                {
+                    Console.Write(x * y + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
