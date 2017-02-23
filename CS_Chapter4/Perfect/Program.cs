36c7acc611120af20d4e0fc2eab65383fe435505
washburns1021 <washburns1021@gmail.com>
2017-02-23 15:05:52 -0500

    perfect

--- CS_Chapter4/Perfect/Program.cs
+++ CS_Chapter4/Perfect/Program.cs
@@ -10,7 +10,15 @@ namespace Perfect
     {
         static void Main(string[] args)
         {
-
+            for (int i = 1; i <= 1000; i++)
+            {
+                int runningTotal = 0;
+                for(int j=1; j < i; j++)
+                {
+                    if (i % j == 0) runningTotal += j;
+                }
+                if (runningTotal == i) Console.WriteLine(i);
+            }
         }
     }
 }
